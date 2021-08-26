b79b4f6b3b6d38522a9cdb7836d24cf2db52d3b2
Narasimhuluv <vasamnarasimhulu123@gmail.com>
2021-08-26 21:23:28 +0530

    add files

--- block-BNaael/api-login/app.js
+++ block-BNaael/api-login/app.js
@@ -3,22 +3,20 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
-var mongoose =  require('mongoose');
+var mongoose = require('mongoose')
 
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
-mongoose.connect('mongodb://localhost/login-App', 
-  { useNewUrlParser: true,  useUnifiedTopology: true }, 
-  (err) => {
-    console.log(err ? err : "Connected to Database");
+mongoose.connect('mongodb://localhost/api-login',{useNewUrlParser : true , useUnifiedTopology : true},(err) => {
+  console.log(err ? err : "Connected to Server")
 })
 
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set('view engine', 'ejs');
 
 app.use(logger('dev'));
 app.use(express.json());
@@ -26,8 +24,8 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/api', indexRouter);
-app.use('/api/users', usersRouter);
+app.use('/', indexRouter);
+app.use('/users', usersRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
